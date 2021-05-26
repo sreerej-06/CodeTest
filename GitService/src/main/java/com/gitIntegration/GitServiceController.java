0e2d2982b524753e58674b333568497c4fb72489
Sreerej <sree@gmail.com>
2021-05-26 22:24:30 +0530

    data

--- GitService/src/main/java/com/gitIntegration/GitServiceController.java
+++ GitService/src/main/java/com/gitIntegration/GitServiceController.java
@@ -22,93 +22,12 @@ public class GitServiceController {
 	private static final String REMOTE_URL = "https://github.com/sreerej-06/TestCommit.git";
 	
 	@PostMapping("/CloneGitRepository")
-	public Persons greeting(Model model) {
+	public String greeting(Model model) {
 
-		Persons S = new Persons(3, "RamHanuman");
-		File localPath = new File ("F:\\Test");
-		/*
-		 * try {
-		 * 
-		 * 
-		 * if(!localPath.delete()) { throw new
-		 * IOException("Could not delete temporary file " + localPath); }
-		 */
-			// then clone
-			System.out.println("Cloning from " + REMOTE_URL + " to " + localPath);
-			try (Git result = Git.cloneRepository().setURI(REMOTE_URL).setDirectory(localPath)
-					.setProgressMonitor(new SimpleProgressMonitor()).call()) {
-				// Note: the call() returns an opened repository already which needs to be
-				// closed to avoid file handle leaks!
-				System.out.println("Having repository: " + result.getRepository().getDirectory());
-			} catch (InvalidRemoteException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (TransportException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (GitAPIException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			// clean up here to not keep using more and more disk-space for these samples
-			/* FileUtils.deleteDirectory(localPath); */
-		/*
-		 * } catch (IOException e) { // TODO Auto-generated catch block
-		 * e.printStackTrace(); }
-		 */
-		return S;
-	}
-
-	@PostMapping("/Commit")
-	public Persons Commit(Model model) {
-		/* String URL = "https://github.com/sreerej-06/TestCommit.git"; */
-		File localPath = new File ("F:\\Test");
-		Persons S = new Persons(3, "RamHanuman");
-		   try {
-			Git git = Git.open(localPath);
-			git.add().addFilepattern("testfile.txt").call();
-			git.commit().setMessage("test commit").call();
-			 PushCommand pushCommand = git.push();
-			    pushCommand.setCredentialsProvider(new UsernamePasswordCredentialsProvider("sreerej.r.k@gmail.com", "Rajakumar@06"));
-			    // you can add more settings here if needed
-			    pushCommand.call();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (GitAPIException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
-		return S;
-		
+		return "Sree";
 	}
-	private static class SimpleProgressMonitor implements ProgressMonitor {
-		@Override
-		public void start(int totalTasks) {
-			System.out.println("Starting work on " + totalTasks + " tasks");
-		}
 
-		@Override
-		public void beginTask(String title, int totalWork) {
-			System.out.println("Start " + title + ": " + totalWork);
-		}
-
-		@Override
-		public void update(int completed) {
-			System.out.print(completed + "-");
-		}
-
-		@Override
-		public void endTask() {
-			System.out.println("Done");
-		}
-
-		@Override
-		public boolean isCancelled() {
-			return false;
-		}
-	}
+	
 
 }
